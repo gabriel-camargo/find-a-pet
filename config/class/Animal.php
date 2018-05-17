44c3417cb39b19e4c097d2312e89fcc28a89ed94
alusantos <analuizaramalho9@gmail.com>
2018-05-17 19:57:08 -0300

    Classe animal

---
+++ config/class/Animal.php
@@ -0,0 +1,191 @@
+<?php
+
+class Animal{
+  private $ani_id;
+  private $ani_nome;
+  private $ani_faixa_etaria;
+  private $ani_sexo;
+  private $ani_informacoes;
+  private $ani_foto;
+  private $ani_status;
+  private $usu_id;
+  private $rac_id;
+  private $ani_dt_hr;
+
+  //get e set ID
+  public function getIdAnimal(){
+    return $this->ani_id;
+  }
+  public function setIdAnimal($value){
+    $this->ani_id = $value;
+  }
+  //get e set NOME
+  public function getNomeAnimal(){
+    return $this->ani_nome;
+  }
+  public function setNomeAnimal($value){
+    $this->ani_nome = $value;
+  }
+  //get e set FAIXA ETARIA
+  public function getFaixaAnimal(){
+    return $this->ani_faixa_etaria;
+  }
+  public function setFaixaAnimal($value){
+    $this->ani_faixa_etaria = $value;
+  }
+  //get e set SEXO
+  public function getSexoAnimal(){
+    return $this->ani_sexo;
+  }
+  public function setSexoAnimal($value){
+    $this->ani_sexo = $value;
+  }
+  //get e set INFORMAÇÕES
+  public function getInformacoesAnimal(){
+    return $this->ani_informacoes;
+  }
+  public function setInformacoesAnimal($value){
+    $this->ani_informacoes = $value;
+  }
+  //get e set FOTO
+  public function getFotoAnimal(){
+    return $this->ani_foto;
+  }
+  public function setFotoAnimal($value){
+    $this->ani_foto = $value;
+  }
+  //get e set STATUS
+  public function getStatusAnimal(){
+    return $this->ani_status;
+  }
+  public function setStatusAnimal($value){
+    $this->ani_status = $value;
+  }
+  //get e set USUARIO ID
+  public function getIdUsuario(){
+    return $this->usu_id;
+  }
+  public function setIdUsuario($value){
+    $this->usu_id = $value;
+  }
+  //get e set RAÇA ID
+  public function getIdRaca(){
+    return $this->rac_id;
+  }
+  public function setIdRaca($value){
+    $this->rac_id = $value;
+  }
+  //get e set DATA HORA
+  public function getDataHoraAnimal(){
+    return $this->ani_dt_hr;
+  }
+  public function setDataHoraAnimal($value){
+    $this->ani_dt_hr = $value;
+  }
+
+
+
+  public function setData($row){
+    $this->setIdAnimal($row['ani_id']);
+    $this->setNomeAnimal($row['ani_nome']);
+    $this->setFaixaAnimal($row['ani_faixa_etaria']);
+    $this->setSexoAnimal($row['ani_sexo']);
+    $this->setInformacoesAnimal($row['ani_informacoes']);
+    $this->setFotoAnimal($row['ani_foto']);
+    $this->setStatusAnimal($row['ani_status']);
+    $this->setIdUsuario($row['usu_id']);
+    $this->setIdRaca($row['rac_id']);
+    $this->setDataHoraAnimal(new DateTime($row['ani_dt_hr']));
+  }
+
+
+  //Função para retornar os dados de um usuário pelo seu ID
+  public function loadById($id){
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM cad_animais WHERE ani_id = :ID", array(
+      ":ID"=>$id
+    ));
+
+    if (count($results) >= 1) {
+      $row = $results[0];
+
+      $this->setData($row);
+    }
+  }
+
+
+
+  public function insert(){
+    $sql = new Sql();
+
+    $results = $sql->select("INSERT INTO cad_animais(
+      ani_nome,
+      ani_faixa_etaria,
+      ani_sexo,
+      ani_informacoes,
+      ani_foto,
+      ani_status,
+      usu_id,
+      rac_id)
+      VALUES(
+        :NOME,
+        :FAIXA_ETARIA,
+        :SEXO,
+        :INFORMACOES,
+        :FOTO,
+        :STATUS,
+        :USUARIO,
+        :RACA)",
+      array(
+      ":NOME" => $this->getNomeAnimal(),
+      ":FAIXA_ETARIA" => $this->getFaixaAnimal(),
+      ":SEXO" => $this->getSexoAnimal(),
+      ":INFORMACOES" => $this->getInformacoesAnimal(),
+      ":FOTO" => $this->getFotoAnimal(),
+      ":STATUS" => $this->getStatusAnimal(),
+      ":USUARIO" => $this->getIdUsuario(),
+      ":RACA" => $this->getIdRaca()
+    ));
+
+    if (count($results) >= 1) {
+      $row = $results[0];
+      $this->setData($row);
+    }
+  }
+
+
+
+
+    public function __construct($nome = "", $faixa = "", $sexo = "", $informacoes = "", $foto = "", $status = "",
+     $usuario = "", $raca = ""){
+
+      $this->setNomeAnimal($nome);
+      $this->setFaixaAnimal($faixa);
+      $this->setSexoAnimal($sexo);
+      $this->setInformacoesAnimal($informacoes);
+      $this->setFotoAnimal($foto);
+      $this->setStatusAnimal($status);
+      $this->setIdUsuario($usuario);
+      $this->setIdRaca($raca);
+    }
+
+    //a função toString é chamada ao tentar imprimir um objeto
+    public function __toString(){
+      return json_encode(array(
+        "ani_id" => $this->getIdAnimal(),
+        "ani_nome" => $this->getNomeAnimal(),
+        "ani_faixa_etaria" => $this->getFaixaAnimal(),
+        "ani_sexo" => $this->getSexoAnimal(),
+        "ani_informacoes" => $this->getInformacoesAnimal(),
+        "ani_foto" => $this->getFotoAnimal(),
+        "ani_status" => $this->getStatusAnimal(),
+        "usu_id" => $this->getIdUsuario(),
+        "rac_id" => $this->getIdRaca(),
+        "ani_dt_hr" => $this->getDataHoraAnimal()
+      ));
+    }
+ }
+
+
+
+  ?>
--- pages/navbar-deslogado.php
+++ pages/navbar-deslogado.php
@@ -1,7 +1,7 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #43A047;">
   <div class="container">
     <!-- Brand: Título principal -->
-    <a class="navbar-brand" href="#"> <strong> Find a Pet </strong></a>
+    <a class="navbar-brand" href="index.php"> <strong> Find a Pet </strong></a>
 
     <!-- Colapse principal -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerPrincipal" aria-controls="navbarTogglerPrincipal" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,8 +11,8 @@
     <!-- Conteudo dentro do colapse principal -->
     <div class="collapse navbar-collapse" id="navbarTogglerPrincipal">
       <ul class="navbar-nav ml-auto">
-        <li><a class="nav-link" href="#"> Registrar-se </a></li>
-        <li><a class="nav-link" href="#"> Logar </a></li>
+        <li><a class="nav-link" href="cadastro.php"> Registrar-se </a></li>
+        <li><a class="nav-link" href="login.php"> Logar </a></li>
       </ul>
 
 
