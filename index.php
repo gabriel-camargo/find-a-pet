efd1547f7719c68c598210e31c97371609c3572e
Gabriel <gabriel.rodrigocam@gmail.com>
2018-06-11 10:56:10 -0300

    checkbox na sidebar

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 	            <!-- /END Separator -->
 
 	           <!-- Menu with submenu -->
+
 	           <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
                    	<div class="d-flex w-100 justify-content-start align-items-center">
 
@@ -53,41 +54,81 @@
                <a href="#submenuCachorro" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
                        <div class="d-flex w-100 justify-content-start align-items-center">
 
-                         <span class="fa fa-dashboard fa-fw mr-3"></span>
+                       <!-- <span class="fa fa-dashboard fa-fw mr-3"></span> -->
                        <span class="menu-collapsed">Cachorro</span>
-                       <span class="submenu-icon ml-auto"></span>
+                       <!-- <span class="submenu-icon ml-auto"></span> -->
 
                      </div>
               </a>
               <div id="submenuCachorro" class="collapse sidebar-submenu">
 
+                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                  <div class="custom-control custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" name="raca-cachorro" id="check-todos-cachorro">
+                    <label class="custom-control-label" for="check-todos-cachorro"><span class="menu-collapsed">Todos</span></label>
+                  </div>
+                </a>
+
                   <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                      <span class="menu-collapsed">Viralata</span>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input check-cachorro" type="checkbox" name="raca-cachorro" value="vira-lata" id="vira-lata">
+                      <label class="custom-control-label" for="vira-lata"><span class="menu-collapsed">Viralata</span></label>
+                    </div>
                   </a>
 
                   <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                        <span class="menu-collapsed">Dalmata</span>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input check-cachorro" type="checkbox" name="raca-cachorro" value="dalmata" id="dalmata">
+                      <label class="custom-control-label" for="dalmata"><span class="menu-collapsed">Dalmata</span></label>
+                    </div>
+                  </a>
+
+                  <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input check-cachorro" type="checkbox" name="raca-cachorro" value="puldo" id="puldo">
+                      <label class="custom-control-label" for="puldo"><span class="menu-collapsed">Puldo</span></label>
+                    </div>
                   </a>
               </div>
 
               <a href="#submenuGato" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
                       <div class="d-flex w-100 justify-content-start align-items-center">
 
-                        <span class="fa fa-dashboard fa-fw mr-3"></span>
+                        <!-- <span class="fa fa-dashboard fa-fw mr-3"></span> -->
                       <span class="menu-collapsed">Gato</span>
-                      <span class="submenu-icon ml-auto"></span>
+                      <!-- <span class="submenu-icon ml-auto"></span> -->
 
                     </div>
              </a>
              <div id="submenuGato" class="collapse sidebar-submenu">
 
-                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                     <span class="menu-collapsed">Viralata de Gato</span>
-                 </a>
+               <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                 <div class="custom-control custom-checkbox">
+                   <input class="custom-control-input" type="checkbox" name="raca-gato" id="check-todos-gato">
+                   <label class="custom-control-label" for="check-todos-gato"><span class="menu-collapsed">Todos</span></label>
+                 </div>
+               </a>
 
-                 <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                       <span class="menu-collapsed">Alguma raça de gato</span>
-                 </a>
+               <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                 <div class="custom-control custom-checkbox">
+                   <input class="custom-control-input check-gato" type="checkbox" name="raca-gato" value="vira-lata-gato" id="vira-lata-gato">
+                   <label class="custom-control-label" for="vira-lata-gato"><span class="menu-collapsed">Vira-lata de gato</span></label>
+                 </div>
+               </a>
+
+               <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                 <div class="custom-control custom-checkbox">
+                   <input class="custom-control-input check-gato" type="checkbox" name="raca-gato" value="gatineo" id="gatineo">
+                   <label class="custom-control-label" for="gatineo"><span class="menu-collapsed">gatineo</span></label>
+                 </div>
+               </a>
+
+               <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                 <div class="custom-control custom-checkbox">
+                   <input class="custom-control-input check-gato" type="checkbox" name="raca-gato" value="rei-do-mundo" id="rei-do-mundo">
+                   <label class="custom-control-label" for="rei-do-mundo"><span class="menu-collapsed">Rei do mundo</span></label>
+                 </div>
+               </a>
              </div>
 
 
@@ -105,13 +146,19 @@
             <!-- Submenu content -->
 	          <div id="submenu2" class="collapse sidebar-submenu">
 
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Macho</span>
-	              </a>
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="sexo" value="m" id="macho">
+                  <label class="custom-control-label" for="macho"><span class="menu-collapsed">Macho</span></label>
+                </div>
+              </a>
 
-	              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-	                    <span class="menu-collapsed">Fêmea</span>
-                </a>
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="sexo" value="f" id="femea">
+                  <label class="custom-control-label" for="femea"><span class="menu-collapsed">Femea</span></label>
+                </div>
+              </a>
             </div>
 
             <a href="#submenu3" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
@@ -125,13 +172,19 @@
             <!-- Submenu content -->
 	          <div id="submenu3" class="collapse sidebar-submenu">
 
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Em adoção</span>
-	              </a>
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="status" value="adocao" id="adocao">
+                  <label class="custom-control-label" for="adocao"><span class="menu-collapsed">Em adoção</span></label>
+                </div>
+              </a>
 
-	              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-	                    <span class="menu-collapsed">Perdido</span>
-                </a>
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="status" value="perdido" id="perdido">
+                  <label class="custom-control-label" for="perdido"><span class="menu-collapsed">Perdido</span></label>
+                </div>
+              </a>
             </div>
 
             <a href="#submenu4" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
@@ -145,18 +198,27 @@
             <!-- Submenu content -->
 	          <div id="submenu4" class="collapse sidebar-submenu">
 
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Filhote</span>
-	              </a>
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="faixa" value="filhote" id="filhote">
+                  <label class="custom-control-label" for="filhote"><span class="menu-collapsed">Filhote</span></label>
+                </div>
+              </a>
 
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-	                    <span class="menu-collapsed">Normal</span>
-                </a>
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="faixa" value="normal" id="normal">
+                  <label class="custom-control-label" for="normal"><span class="menu-collapsed">Normal</span></label>
+                </div>
+              </a>
 
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-	                    <span class="menu-collapsed">Velho</span>
-                </a>
-            </div>            
+              <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" name="faixa" value="velho" id="velho">
+                  <label class="custom-control-label" for="velho"><span class="menu-collapsed">Velho</span></label>
+                </div>
+              </a>
+            </div>
 
             <!-- Logo -->
 	            <li class="list-group-item logo-separator d-flex justify-content-center">
@@ -228,6 +290,24 @@
             // Collapse/Expand icon
             $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
           }
+
+          var checkTodosCachorro = $("#check-todos-cachorro");
+          checkTodosCachorro.click(function () {
+            if ( $(this).is(':checked') ){
+              $('.check-cachorro').prop("checked", true);
+            }else{
+              $('.check-cachorro').prop("checked", false);
+            }
+          });
+
+          var checkTodosGato = $("#check-todos-gato");
+          checkTodosGato.click(function () {
+            if ( $(this).is(':checked') ){
+              $('.check-gato').prop("checked", true);
+            }else{
+              $('.check-gato').prop("checked", false);
+            }
+          });
       </script>
   </body>
 </html>
