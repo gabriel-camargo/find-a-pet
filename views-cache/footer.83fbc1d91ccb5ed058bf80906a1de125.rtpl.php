ee530515cc0e81253ab37648077bd5c744a6dd64
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2019-01-02 22:00:15 -0200

    Delete footer.83fbc1d91ccb5ed058bf80906a1de125.rtpl.php

--- views-cache/footer.83fbc1d91ccb5ed058bf80906a1de125.rtpl.php
+++
@@ -1,3 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><script type="text/javascript" src="/res/js/vendor/jQuery/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="/res/js/vendor/bootstrap/bootstrap.js"></script>
-<script type="text/javascript" src="/res/js/vendor/bootstrap/bootstrap.bundle.js"></script> 
