82cfbac6fcf36218eb4bc18bc631a362971ce1bc
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2019-01-29 21:49:40 -0200

    fix - scripts

--- scripts/bd_findapet.sql
+++ scripts/bd_findapet.sql
@@ -18,7 +18,6 @@ USE `bd_findapet`;
 
 -- Exportação de dados foi desmarcado.
 -- Copiando estrutura para tabela bd_findapet.tbl_especies
-DROP TABLE IF EXISTS `tbl_especies`;
 CREATE TABLE IF NOT EXISTS `tbl_especies` (
   `esp_id` int(11) NOT NULL AUTO_INCREMENT,
   `esp_nome` varchar(45) NOT NULL,
@@ -28,37 +27,34 @@ CREATE TABLE IF NOT EXISTS `tbl_especies` (
 
 -- Exportação de dados foi desmarcado.
 -- Copiando estrutura para tabela bd_findapet.tbl_faixa_etaria
-DROP TABLE IF EXISTS `tbl_faixa_etaria`;
 CREATE TABLE IF NOT EXISTS `tbl_faixa_etaria` (
   `fai_id` int(11) NOT NULL AUTO_INCREMENT,
   `fai_nome` varchar(45) NOT NULL,
   PRIMARY KEY (`fai_id`),
   UNIQUE KEY `fai_id_UNIQUE` (`fai_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- Exportação de dados foi desmarcado.
--- Copiando estrutura para tabela bd_findapet.tbl_porte
-DROP TABLE IF EXISTS `tbl_porte`;
-CREATE TABLE IF NOT EXISTS `tbl_porte` (
+-- Copiando estrutura para tabela bd_findapet.tbl_portes
+CREATE TABLE IF NOT EXISTS `tbl_portes` (
   `por_id` int(11) NOT NULL AUTO_INCREMENT,
   `por_nome` varchar(45) NOT NULL,
   PRIMARY KEY (`por_id`),
   UNIQUE KEY `por_id_UNIQUE` (`por_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- Exportação de dados foi desmarcado.
 -- Copiando estrutura para tabela bd_findapet.tbl_status
-DROP TABLE IF EXISTS `tbl_status`;
 CREATE TABLE IF NOT EXISTS `tbl_status` (
   `sta_id` int(11) NOT NULL AUTO_INCREMENT,
   `sta_nome` varchar(45) NOT NULL,
+  `sta_tipo` varchar(20) NOT NULL,
   PRIMARY KEY (`sta_id`),
   UNIQUE KEY `sta_id_UNIQUE` (`sta_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- Exportação de dados foi desmarcado.
 -- Copiando estrutura para tabela bd_findapet.tbl_usuarios
-DROP TABLE IF EXISTS `tbl_usuarios`;
 CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
   `usu_id` int(11) NOT NULL AUTO_INCREMENT,
   `usu_nome` varchar(45) NOT NULL,
@@ -70,28 +66,32 @@ CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
 
 -- Exportação de dados foi desmarcado.
 -- Copiando estrutura para tabela bd_findapet.tbl_animais
-DROP TABLE IF EXISTS `tbl_animais`;
 CREATE TABLE IF NOT EXISTS `tbl_animais` (
   `ani_id` int(11) NOT NULL AUTO_INCREMENT,
   `ani_nome` varchar(45) NOT NULL,
   `ani_sexo` varchar(1) NOT NULL,
-  `ani_status` tinyint(4) NOT NULL,
-  `ani_faixa_etaria` tinyint(4) NOT NULL,
-  `ani_porte` tinyint(11) NOT NULL,
   `ani_informacoes` mediumtext,
   `ani_dt_hr` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `usu_id` int(11) NOT NULL,
   `esp_id` int(11) NOT NULL,
+  `fai_id` int(11) NOT NULL,
+  `por_id` int(11) NOT NULL,
+  `sta_id` int(11) NOT NULL,
   PRIMARY KEY (`ani_id`),
   UNIQUE KEY `ani_id_UNIQUE` (`ani_id`),
   KEY `fk_cad_animais_cad_usuarios1_idx` (`usu_id`),
   KEY `fk_esp_id` (`esp_id`),
+  KEY `fk_fai_id` (`fai_id`),
+  KEY `fk_por_id` (`por_id`),
+  KEY `fk_sta_id` (`sta_id`),
   CONSTRAINT `fk_cad_animais_cad_usuarios1` FOREIGN KEY (`usu_id`) REFERENCES `tbl_usuarios` (`usu_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_esp_id` FOREIGN KEY (`esp_id`) REFERENCES `tbl_especies` (`esp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_esp_id` FOREIGN KEY (`esp_id`) REFERENCES `tbl_especies` (`esp_id`),
+  CONSTRAINT `fk_fai_id` FOREIGN KEY (`fai_id`) REFERENCES `tbl_faixa_etaria` (`fai_id`),
+  CONSTRAINT `fk_por_id` FOREIGN KEY (`por_id`) REFERENCES `tbl_portes` (`por_id`),
+  CONSTRAINT `fk_sta_id` FOREIGN KEY (`sta_id`) REFERENCES `tbl_status` (`sta_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 
 -- Copiando estrutura para tabela bd_findapet.tbl_adocoes
-DROP TABLE IF EXISTS `tbl_adocoes`;
 CREATE TABLE IF NOT EXISTS `tbl_adocoes` (
   `ado_id` int(11) NOT NULL AUTO_INCREMENT,
   `ado_dt_hr` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -106,6 +106,7 @@ CREATE TABLE IF NOT EXISTS `tbl_adocoes` (
   CONSTRAINT `fk_adocoes_cad_usuarios1` FOREIGN KEY (`usu_doador_id`) REFERENCES `tbl_usuarios` (`usu_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_adocoes_cad_usuarios2` FOREIGN KEY (`usu_adotador_id`) REFERENCES `tbl_usuarios` (`usu_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- Exportação de dados foi desmarcado.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
