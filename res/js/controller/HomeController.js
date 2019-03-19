2467d26dddc773cbf9d07545f6ab4d1a14f1fdec
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2019-03-19 20:26:47 -0300

    filtro de sexo

--- res/js/controller/HomeController.js
+++ res/js/controller/HomeController.js
@@ -2,6 +2,7 @@ class HomeController{
 
     constructor(){
         this._inputListEspecie = Array.from(document.querySelectorAll(".check-especie"));
+        this._inputListSexo = Array.from(document.querySelectorAll(".check-sexo"));
 
         this._http = new HttpService();
         this._view = new HomeView(document.querySelector('#view-publicacoes'));
@@ -32,13 +33,28 @@ class HomeController{
 
         if(inputListEspecieChecked.length >= 1) {
 
-            filter += "and ( ";
+            filter += " and ( ";
 
             for (let index = 0; index < inputListEspecieChecked.length; index++) {
 
                 filter += `t1.esp_id = ${inputListEspecieChecked[index].value}`;
 
                 if( ( index + 1) != inputListEspecieChecked.length) filter +=" or ";
+                else filter += ")";            
+            }
+        }
+
+        const inputListSexoChecked = this._inputListSexo.filter(el => el.checked);
+
+        if(inputListSexoChecked.length >= 1) {
+
+            filter += " and ( ";
+
+            for (let index = 0; index < inputListSexoChecked.length; index++) {
+
+                filter += `t1.ani_sexo = ${inputListSexoChecked[index].value}`;
+
+                if( ( index + 1) != inputListSexoChecked.length) filter +=" or ";
                 else filter += ")";            
             }
         }
